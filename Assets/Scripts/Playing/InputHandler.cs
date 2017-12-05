b4e87bd6a981dcabd0a3ee82876ea201d9d2a2b2
Benson Poli <benson_polican@dlsu.edu.ph>
2017-12-06 02:26:21 +0800

    Swipe Gesture

--- Assets/Scripts/Playing/InputHandler.cs
+++ Assets/Scripts/Playing/InputHandler.cs
@@ -2,25 +2,67 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum SwipeDirection{
+	None=0,
+	Left=1,
+	Right=2,
+	Up=4,
+	Down=8
+}
+
 public class InputHandler : MonoBehaviour {
 	// Use this for initialization
-	void Start () {
 
+
+
+	public SwipeDirection Direction{ set; get; }
+
+	private Vector3 touchPosition;
+	private float swipeResistance = 50.0f;
+
+
+	void Start () {
 	}
 
+	public bool isSwiping(SwipeDirection direction){
+		if (this.Direction == direction)
+			return true;
+		else
+			return false;
+	}
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKeyDown(KeyCode.W)) {
+
+		Direction = SwipeDirection.None;
+
+		if (Input.GetMouseButtonDown (0)) {
+			touchPosition = Input.mousePosition;
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Vector2 deltaSwipe = touchPosition - Input.mousePosition;
+
+			if (Mathf.Abs (deltaSwipe.x) > swipeResistance) {
+				Direction |=(deltaSwipe.x<0) ? SwipeDirection.Right:SwipeDirection.Left;
+			}
+			if (Mathf.Abs (deltaSwipe.y) > swipeResistance) {
+				Direction |=(deltaSwipe.y<0) ? SwipeDirection.Up:SwipeDirection.Down;
+
+			}
+		}
+
+
+
+		if (Input.GetKeyDown(KeyCode.W)||this.isSwiping(SwipeDirection.Up)) {
 			EventBroadcaster.Instance.PostEvent (EventNames.ON_KEY_PRESSED_W);
 		}
-		else if (Input.GetKeyDown(KeyCode.A)) {
+		else if (Input.GetKeyDown(KeyCode.A)||this.isSwiping(SwipeDirection.Left)) {
 			EventBroadcaster.Instance.PostEvent (EventNames.ON_KEY_PRESSED_A);
 		}
-		else if (Input.GetKeyDown(KeyCode.D)) {
+		else if (Input.GetKeyDown(KeyCode.D)||this.isSwiping(SwipeDirection.Right)) {
 			EventBroadcaster.Instance.PostEvent (EventNames.ON_KEY_PRESSED_D);
 		}
-		else if (Input.GetKeyDown(KeyCode.S)) {
+		else if (Input.GetKeyDown(KeyCode.S)||this.isSwiping(SwipeDirection.Down)) {
 			EventBroadcaster.Instance.PostEvent (EventNames.ON_KEY_PRESSED_S);
 		}
 	}
