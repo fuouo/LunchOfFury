8b1c98cfdb9aedabd13676bf266121a21e467ccb
CudoXY <gelo103097@yahoo.com>
2017-12-08 21:44:26 +0800

    Refactored frenzy combo gauge

--- Assets/Scripts/Playing/Player.cs
+++ Assets/Scripts/Playing/Player.cs
@@ -8,52 +8,17 @@ public class Player : MonoBehaviour {
 	[SerializeField] PlayerType type;
 
 
-	[Header("COMBO")]
-	[SerializeField] float minimumComboForFrenzy = 40.0f;
-	[SerializeField] float frenzyDecayRate = 0.05f;
-	[SerializeField] float comboIncrementRate =1;
-	private float comboPoints;
-
-	[Header("STATES")]
-	[SerializeField] private GameObject gameOverPanel;
-	private bool alive;
-
-
 	// Use this for initialization
 	void Start () {
-		EventBroadcaster.Instance.AddObserver (EventNames.ENEMY_PUNCHED, this.enemyPunched);
 		EventBroadcaster.Instance.AddObserver (EventNames.ON_SWIPE, this.punch);
-		EventBroadcaster.Instance.AddObserver (EventNames.PLAYER_DEATH, this.gameOver);
-		comboPoints = 0;
-		alive = true;
 
 		GetComponent<SpriteRenderer> ().sprite = type.defaultSprite;
 		GetComponent<Animator> ().runtimeAnimatorController = type.animator;
 
 	}
 
-	void updateCombo(){
-
-		Debug.Log ("WENT IN START");
-
-		Parameters parameters = new Parameters();
-		parameters.PutExtra(GameManager.MAX_COMBO_KEY, minimumComboForFrenzy);
-		parameters.PutExtra (GameManager.CURRENT_COMBO_KEY, comboPoints);
-		EventBroadcaster.Instance.PostEvent (EventNames.ON_UPDATE_COMBO_UI,parameters);
-	}
-
 	// Update is called once per frame
 	void Update () {
-		//		scoreText.text = currentScore.ToString ();
-		if (comboPoints > 0) {
-			comboPoints -= frenzyDecayRate;
-			//			comboGauge.value = comboPoints;
-			updateCombo();
-		}
-
-		if (comboPoints >= minimumComboForFrenzy) {
-			this.frenzy ();
-		}
 	}
 
 	void punch(Parameters parameters){
@@ -66,34 +31,7 @@ public class Player : MonoBehaviour {
 //
 //		Debug.Log (direction);
 	}	
-
-
-
-	public bool isAlive(){
-		return this.alive;
-	}
-
-	public void enemyPunched(){
-		comboPoints=comboPoints+comboIncrementRate;
-		//		comboGauge.value = comboPoints;
-	}
-
-	public void gameOver(){
-		alive = false;
-		gameOverPanel.SetActive (true);
-		gameOverPanel.transform.SetSiblingIndex (9999);
-
-	}
-
-	public void onClickPlayAgain(){
-		//LoadManager.Instance.LoadScene (SceneNames.GAME_SCENE);	
-	}
-
-	private void frenzy(){
-
-		comboPoints = 0;
-		EventBroadcaster.Instance.PostEvent (EventNames.FRENZY_TRIGGERED);
-	}
+	
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
