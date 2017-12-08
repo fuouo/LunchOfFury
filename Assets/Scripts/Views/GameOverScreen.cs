672e11db445fc7dc4e83a53dc6cda47bca28b866
Benson Poli <benson_polican@dlsu.edu.ph>
2017-12-08 23:46:09 +0800

    Shop at GameOver

--- Assets/Scripts/Views/GameOverScreen.cs
+++ Assets/Scripts/Views/GameOverScreen.cs
@@ -10,9 +10,20 @@ public class GameOverScreen : View {
 	[SerializeField] Text BestGold;
 	[SerializeField] Button PlayButton;
 
+
+	[SerializeField] Text price;
+
+
+	[SerializeField] Image SelectedSprite;
+
+	[SerializeField] Player CurrentlyBeingUsed;
+
+	PlayerType[] playerTypes;
+	int currentCounter =0;
 	// Use this for initialization
 	void Start () {
 
+		playerTypes = PlayerType.LoadAll ();
 		EarnedGold.text = GameManager.Instance.EarnedGold + "";
 		CurrentGoldEarned.text = GameManager.Instance.CurrentGold + "";
 		BestGold.text = GameManager.Instance.BestGold + "";
@@ -24,6 +35,19 @@ public class GameOverScreen : View {
 	void Update () {
 
 	}
+	public void OnBuy(){
+		if (playerTypes [currentCounter].locked == false) {
+			this.OnPlay ();
+		}else{
+			if (playerTypes [currentCounter].price < GameManager.Instance.getEarnedGold()) {
+				GameManager.Instance.setEarnedGold (GameManager.Instance.getEarnedGold() - playerTypes [currentCounter].price);
+				//Switch Player
+
+
+
+			}
+		}
+	}
 
 
 	public void OnPlay() {
@@ -34,5 +58,47 @@ public class GameOverScreen : View {
 		//Notify GameManager that game starts
 		EventBroadcaster.Instance.PostEvent (EventNames.ON_PLAY);
 
+	}
+
+
+	public void OnClickPlayerSkinRight(){
+		//		SelectedSprite.sprite;
+		EventBroadcaster.Instance.PostEvent (EventNames.BUTTON_CLICK);
+		if (currentCounter + 1 == playerTypes.Length)
+			currentCounter = 0;
+		else
+			currentCounter++;
+
+		SelectedSprite.sprite = playerTypes [currentCounter].defaultSprite;
+		if (playerTypes [currentCounter].locked == false) {
+			price.text = "";
+			//SWITCH PLAYER
+
+
+
+		} else {
+			price.text = playerTypes [currentCounter].price + " Coins";
+		}
+
+	}
+	public void OnClickPlayerSkinLeft(){
+		EventBroadcaster.Instance.PostEvent (EventNames.BUTTON_CLICK);
+		if (currentCounter - 1 == -1)
+			currentCounter = playerTypes.Length - 1;
+		else
+			currentCounter--;
+
+		SelectedSprite.sprite = playerTypes [currentCounter].defaultSprite;
+
+		if (playerTypes [currentCounter].locked == false) {
+			price.text = "";
+			//SWITCH PLAYER
+
+
+
+		} else {
+			price.text = playerTypes [currentCounter].price + " Coins";
+
+		}
 	}
 }
